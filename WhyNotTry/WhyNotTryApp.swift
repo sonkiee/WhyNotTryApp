13d2a7ded67a6395d7459a2f5c4409a8f6319dbb
sonkiee <annagu.kennedy@gmail.com>
2025-11-02 08:04:38 +0100

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by sonkey on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
